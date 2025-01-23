862cab40fb607d950801a6d116d91cd1970ec84f
guilherme seixas <guilhermesantoseixas@outlook.com>
2025-01-23 18:19:32 -0300

    Configured test application properties and created Game entity

---
+++ src/main/java/com/guilherme/games_api/entity/Game.java
@@ -0,0 +1,36 @@
+package com.guilherme.games_api.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name = "games")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Column(name = "release_year")
+    private Integer releaseYear;
+
+    private String genre;
+
+    private String platform;
+
+    @Column(name = "img_url")
+    private String imgUrl;
+
+    @Column(name = "short_description")
+    private String shortDescription;
+
+    @Column(name = "long_description")
+    private String longDescription;
+}
